a7e5474be00b3d728f345b8d39767eef3737ff34
tommy <54563161+to-mmy@users.noreply.github.com>
2021-01-16 02:57:09 -0800

    tz - update app to get form values

--- app.py
+++ app.py
@@ -1,24 +1,63 @@
 from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class ApiKeys(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    spotifyPlaylistId = db.Column(db.String(200), nullable=False)
+    spotifyClientId = db.Column(db.String(200), nullable=False)
+    spotifyClientSecret = db.Column(db.String(200), nullable=False)
+    youtubeApiKey = db.Column(db.String(200), nullable=False)
+    youtubeClientId = db.Column(db.String(200), nullable=False)
+    youtubeClientSecret = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<ApiKeys(playlistId=%s, spotifyClientId=%s, youtubeApiKey=%s, youtubeClientId=%s)>' % (self.playlistId, self.spotifyClientId, self.youtubeApiKey, self.youtubeClientId)
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return 'hiw'
-
-@app.route('/welcome')
-def welcome():
-    return 'welcome'
-
-@app.route('/login',methods =['GET','POST'])
-def login():
     error = None
-    if request.method == "Post":
-        if request.form['username'] != 'admin' or   request.form['password'] != 'admin':
-            error = 'Invalid credentials'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html',error = error)
+    if request.method == "POST":
+        spotify_playlist_id = request.form.get('spotifyPlaylistId', False)
+        spotify_client_id = request.form.get('spotifyClientId', False)
+        spotify_client_secret = request.form.get('spotifyClientSecret', False)
+        youtube_api_key = request.form.get('youtubeApiKey', False)
+        youtube_client_id = request.form.get('youtubeClientId', False)
+        youtube_client_secret = request.form.get('youtubeClientSecret', False)
+
+        new_api_keys = ApiKeys(spotifyPlaylistId=spotify_playlist_id, spotifyClientId=spotify_client_id, spotifyClientSecret=spotify_client_secret, youtubeApiKey=youtube_api_key, youtubeClientId=youtube_client_id, youtubeClientSecret=youtube_client_secret)
+        try:
+            db.session.add(new_api_keys)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue creating your playlist."
+    else:
+        return render_template('index.html', error = error)
+
+# @app.route('/new', methods=['GET', 'POST'])
+# def login():
+#     error = None
+#     if request.method == "POST":
+#         spotify_playlist_id = request.form['spotifyPlaylistId']
+#         spotify_client_id = request.form['spotifyClientId']
+#         spotify_client_secret = request.form['spotifyClientSecret']
+#         youtube_api_key = request.form['youtubeApiKey']
+#         youtube_client_id = request.form['youtubeClientId']
+#         youtube_client_secret = request.form['youtubeClientSecret']
+
+#         new_api_keys = ApiKeys(spotifyPlaylistId=spotify_playlist_id, spotifyClientId=spotify_client_id, spotifyClientSecret=spotify_client_secret, youtubeApiKey=youtube_api_key, youtubeClientId=youtube_client_id, youtubeClientSecret=youtube_client_secret)
+#         try:
+#             db.session.add(new_api_keys)
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return "There was an issue creating your playlist."
+#     else:
+#         return render_template('new.html', error = error)
 
 if __name__ == "__main__":
     app.run(debug=True)
